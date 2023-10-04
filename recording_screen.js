eae6ac78561bcfd014ea1c7e6324096790c724cf
J Ramakrishna Anand <jranand.py@gmail.com>
2023-10-04 09:02:47 +0530

    Fixed recording cancel window close issue

--- recording_screen.js
+++ recording_screen.js
@@ -18,12 +18,32 @@ const convertBlobToBase64 = (blob) => {
   });
 };
 
+const getMimeType = (MediaRecorder, exportFormat) => {
+  if(exportFormat === 'webm') {
+    if(MediaRecorder.isTypeSupported("video/webm; codecs=vp9")) {
+      return "video/webm; codecs=vp9";
+    }
+    if(MediaRecorder.isTypeSupported("video/webm; codecs=vp8")) {
+      return "video/webm; codecs=vp8";
+    }
+    return "video/webm";
+  } else {
+    return "video/mp4";
+  }
+  
+  
+}
 
-
+const closeWindow = () => {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    window.close();
+  });
+};
  
 chrome.runtime.onMessage.addListener((message) => {
   
   if (message.name !== 'startRecordingOnBackground') {
+    closeWindow();
     return;
   }
 
@@ -32,12 +52,7 @@ chrome.runtime.onMessage.addListener((message) => {
   let blobType = "video/mp4";
   if(message.body.exportFormat) {
     console.log('recordedFormat', message);
-    exportFormat = message.body.exportFormat;
-    if(exportFormat === 'webm') {
-      blobType = "video/webm";  
-    } else if(exportFormat === 'gif') {
-      blobType ="image/gif";
-    }
+    exportFormat = message.body.exportFormat;   
   }
 
   if(message.body.exportSize && message.body.exportSize === 'fullHD') {
@@ -64,10 +79,13 @@ chrome.runtime.onMessage.addListener((message) => {
           optional: videoResolution,
         }
       }).then(stream => {
-        const mediaRecorder = new MediaRecorder(stream);
+        
+        const mediaRecorder = new MediaRecorder(stream, {
+          videoBitsPerSecond: 3000000, // Adjust the bitrate as needed for higher quality
+          audioBitsPerSecond: 128000 // Adjust the audio bitrate as needed
+        });
         let chunks = [];
-       
-       
+        blobType = getMimeType(MediaRecorder, exportFormat);
         mediaRecorder.ondataavailable = function(e) {
           chunks.push(e.data);
           /*if(exportFormat === 'gif') {
@@ -88,26 +106,13 @@ chrome.runtime.onMessage.addListener((message) => {
            
           //const base64 = await fetchBlob(URL.createObjectURL(blobFile));
           const url = URL.createObjectURL(blobFile);
-          if(exportFormat !== 'gif'){
-            chrome.downloads.download({
-              url: url,
-              filename: 'screen_recording.'+ exportFormat,
-              saveAs: true
-            });
-          } 
+          chrome.downloads.download({
+            url: url,
+            filename: 'screen_recording.'+ exportFormat,
+            saveAs: true
+          });
           // When recording is finished, send message to current tab content script with the base64 video
-          /*chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-            const tabWhenRecordingStopped = tabs[0];
-
-            chrome.tabs.sendMessage(tabWhenRecordingStopped.id, {
-              name: 'endedRecording',
-              body: {
-                base64,
-              }
-            })
-
-            window.close();
-          });*/
+          closeWindow();
 
           // Stop all tracks of stream
           stream.getTracks().forEach(track => track.stop());
@@ -118,6 +123,8 @@ chrome.runtime.onMessage.addListener((message) => {
       }).finally(async () => {
         // After all setup, focus on previous tab (where the recording was requested)
         await chrome.tabs.update(message.body.currentTab.id, { active: true, selected: true })
+      }).catch(()=>{
+        closeWindow();
       });
-    })
+    });
 });
